4e00dc3da36d3b978204d93d3b83edf9b1b8deda
Elias Najjar <eliasnajjar111@gmail.com>
2025-03-28 15:45:33 -0400

    Add files via upload

---
+++ Infinite_Calculator.py
@@ -0,0 +1,30 @@
+import time
+
+maxExponent = 100000
+
+def setValue(val,exps):
+    while val > 10:
+        val /= 10
+        exps[-1] += 1
+        if exps[-1] > maxExponent:
+            exps.append(exps[-1]-maxExponent)
+            exps[-2] = maxExponent
+    return val
+
+
+def printValue(val,exps):
+    print(val,end="")
+    for x in exps:
+        print(" * 10^" + str(x),end="")
+    print()
+
+step = 1
+value = 1
+exponents = [0]
+while True:
+    value *= step
+    value = setValue(value,exponents)
+    print(str(step) + ": ",end="")
+    printValue(value,exponents)
+    step += 1
+    #time.sleep(.05)
