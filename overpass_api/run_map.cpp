7fb6dd0d211a9e86bdfecc8761f3acf337df4612
EliasNajjar <eliasnajjar111@gmail.com>
2025-12-14 15:03:01 -0500

    updated overpass api to use .exe file instead of python, also new project for math lab with nxm grids

--- overpass_api/run_map.cpp
+++ overpass_api/run_map.cpp
@@ -2,10 +2,12 @@
 
 int main() {
     // windows
-    int result = std::system("python map_from_overpass.py 40.712 -74.0060 >nul 2>&1"); // call python script with arguments latitude, longitude
+    int result = std::system("map_from_overpass.exe 40.712 -74.0060"); // call python script with arguments latitude, longitude
 
     // linux and mac
-    //int result = std::system("python map_from_overpass.py 40.712 -74.0060 >/dev/null 2>&1");
+    //int result = std::system("map_from_overpass.exe 40.712 -74.0060");
+
+    std::cout << result << std::endl;
 
     return 0;
 }
